0e84093294ec7eb3a9039e86c84aca2b8f1a76e1
Manjesh V <manjeshpv@gmail.com>
2018-05-20 21:35:54 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@
  */
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const http = require('http');
 const express = require('express');
 
