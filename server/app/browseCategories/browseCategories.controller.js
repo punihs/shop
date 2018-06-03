521bdf342d44e0b8a2f6f2e89ff63bff90a1fceb
pulse localhost.localdomain <pulse@localhost.localdomain.xyz.com>
2018-06-03 21:19:36 +0530

    fix(service): Categories

--- server/app/browseCategories/browseCategories.controller.js
+++ server/app/browseCategories/browseCategories.controller.js
@@ -1,7 +1,22 @@
-exports.index = (req, res) => {
-  res.render('browseCategories/index', {
-    title: 'aboutpage',
-    meta_disctription: 'discription',
-    meta_title: 'metatital',
-  });
-};
+const rp = require('request-promise');
+const { URLS_MYACCOUNT } = require('../../config/environment');
+const { URLS_API } = require('../../config/environment');
+
+exports.index = (req, res, next) => Promise
+  .all([
+    rp(`${URLS_API}/api/stores`, { json: true }),
+  ])
+  .then(([stores]) =>
+    res.render('browseCategories/index', Object
+      .assign(stores, {
+        URLS_MYACCOUNT,
+        title: 'aboutpage',
+        meta_disctription: 'discription',
+        meta_title: 'metatital',
+        categories: [{
+          id: 1,
+          name: 'Apparels',
+        }],
+      })))
+  .catch(next);
+
