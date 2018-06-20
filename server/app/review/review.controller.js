33836035f0892eb03a349bebfe7a0262b41ccfba
pulse localhost.localdomain <pulse@localhost.localdomain.xyz.com>
2018-06-20 19:31:55 +0530

    feat(feedback): review

--- server/app/review/review.controller.js
+++ server/app/review/review.controller.js
@@ -5,15 +5,20 @@ exports.index = (req, res, next) => Promise
   .all([
     rp(`${URLS_API}/api/reviews`, { json: true }),
   ])
-  .then(([reviews]) =>
+  .then(([reviews]) => {
+    // return res.json(Object
+    //   .assign(reviews, {
+    //   }));
     res
-      .render('review/index', reviews, {
-        title: 'review',
-        meta_description: 'disctription',
-        meta_title: 'title',
-        reviews: {
-          id: 1,
-          review: 'good',
-        },
-      }))
+      .render('review/index', Object
+        .assign(reviews, {
+          title: 'review',
+          meta_description: 'disctription',
+          meta_title: 'title',
+          reviews: {
+            id: 1,
+            review: 'good',
+          },
+        }));
+  })
   .catch(next);
