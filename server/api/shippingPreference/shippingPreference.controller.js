9039dd9452953a1cbcf20a2f166cbf8338807b19
punihs <36591236+punihs@users.noreply.github.com>
2018-07-11 20:51:23 +0530

    fix: shipping preference modified (#122)
    * fix: shipping preference modified
    
    * fix: shipping removed commented lines
    
    * fix: shipping preference spec modified

--- server/api/shippingPreference/shippingPreference.controller.js
+++ server/api/shippingPreference/shippingPreference.controller.js
@@ -1,20 +1,22 @@
 const { ShippingPreference } = require('./../../conn/sqldb');
 
 exports.show = (req, res) => {
-  const { id } = req.params;
   const options = {
-    attributes: ['id', 'is_basic_photo', 'is_advanced_photo', 'is_scan_document', 'is_repacking'],
+    attributes: ['id', 'is_basic_photo', 'is_advanced_photo', 'is_scan_document', 'is_repacking',
+      'is_sticker', 'is_extra_packing', 'is_original_box',
+      'is_gift_wrap', 'is_gift_note', 'max_weight'],
+    where: { customer_id: req.user.id },
   };
   const preference = ShippingPreference
-    .findById(id, options);
-  return res.json(preference);
+    .findAll(options);
+  res.json(preference);
 };
 
 exports.update = async (req, res) => {
   const { id } = req.params;
   const preference = req.body;
   const status = await ShippingPreference
-    .update(preference, { where: { customer_id: id } });
+    .update(preference, { where: { id } });
   return res.json(status);
 };
 
