a57e149a02b2ecf0ea1fe25b03f7e3c89c974e53
pulse localhost.localdomain <pulse@localhost.localdomain.xyz.com>
2018-08-01 22:29:44 +0530

    fix(WWW): review changes

--- server/api/pricing/index.js
+++ server/api/pricing/index.js
@@ -5,7 +5,7 @@ const router = express.Router(); // eslint-disable-line new-cap
 router.get('/', controller.index);
 router.all('/ai', controller.ai);
 router.all('/expressive', controller.expressive);
-router.all('/customerPricing', controller.customerPricing);
-router.all('/shipCalculate', controller.shipCalculate);
+router.get('/customerPricing', controller.customerPricing);
+router.get('/shipCalculate', controller.shipCalculate);
 
 module.exports = router;
