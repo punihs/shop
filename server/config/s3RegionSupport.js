91c039f9bab3b174b701c3a2560dcd6cd12c288b
pulse localhost.localdomain <pulse@localhost.localdomain.xyz.com>
2018-08-07 22:32:37 +0530

    s3support
    (cherry picked from commit d19fdfe)

--- server/config/s3RegionSupport.js
+++ server/config/s3RegionSupport.js
@@ -1,6 +1,9 @@
+const debug = require('debug');
 const geoip = require('geoip-lite');
 
 const { env, PREFIX } = require('../config/environment');
+
+const log = debug('server-config-s3RegionSupport');
 
 const regionsMap = {
   DEFAULT: 'us-west-2',
@@ -14,7 +17,9 @@ module.exports = () => (req, res, next) => {
     req.connection.socket.remoteAddress;
 
   const s3Region = regionsMap[(geoip.lookup(ipAddress) || {}).country] || regionsMap.DEFAULT;
-  req.s3BaseUrl = `${PREFIX}s3.${s3Region}.amazonaws.${env !== 'production' ? 'test' : 'com'}`;
-
+  log('S3Region', s3Region);
+  const devUrl = `http://s3.${s3Region}.amazonaws.test`;
+  const prodUrl = `${PREFIX}s3.${s3Region}.shoppre.com.s3-website.${s3Region}.amazonaws.com`;
+  req.s3BaseUrl = env !== 'production' ? devUrl : prodUrl;
   next();
 };
