5c277c34e888ab258416e1dce2804b7322c0d267
pulse localhost.localdomain <pulse@localhost.localdomain.xyz.com>
2018-08-07 22:36:30 +0530

    fix(www): Reviews pagination

--- server/api/review/review.controller.js
+++ server/api/review/review.controller.js
@@ -1,25 +1,23 @@
 const { Review, Country, Source } = require('./../../conn/sqldb');
 
-exports.index = (req, res, next) => {
-  const options = {
+exports.index = (req, res, next) => Review
+  .findAll({
     attributes: ['id', 'name', 'source_id', 'description', 'rating', 'approved_by', 'is_approved', 'created_at'],
     include: [{
       model: Country,
       attributes: ['name', 'flag'],
-    },
-    {
+    }, {
       model: Source,
       attributes: ['name'],
     }],
     limit: Number(req.query.limit) || 20,
     offset: Number(req.query.offset) || 0,
-  };
-
-  return Review
-    .findAll(options)
-    .then(review => res.json(review))
-    .catch(next);
-};
+  })
+  .then(reviews => res
+    .json({
+      items: reviews,
+    }))
+  .catch(next);
 
 exports.show = (req, res, next) => {
   const { id } = req.params;
