fabb1986273f72ca9a7e4b00ea730b2b0d832c45
Manjesh V <manjesh@quetzal.in>
2018-09-23 17:41:38 +0530

    hotfix(User Registration): password hook calls when blank (#306)

--- server/components/oauth/express/google.js
+++ server/components/oauth/express/google.js
@@ -50,6 +50,7 @@ exports.oauth = (req, res, next) => {
                 salutation: '',
                 first_name: me.name.givenName,
                 last_name: me.name.familyName,
+                hooks: false
               },
             })
             .then((result) => {
