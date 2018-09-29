3fd13615b9a235f3518e5397b3b4466e3ec8f62d
Vikas Kumar <vikasjson@gmail.com>
2018-09-29 12:49:52 +0530

    feat(Sign up) : Welcome email integration (#316)
    feat(Sign up): Welcome Email

--- server/api/user/user.sample.js
+++ server/api/user/user.sample.js
@@ -2,7 +2,11 @@ module.exports = {
   signup: {
     customer: {
       email: 'customer@customer.com',
+      salutation: 'Mr.',
       first_name: 'Vikas',
+      last_name: 'Kumar',
+      locker: 'SHPR20-416',
+      created_at: '13-03-2018',
     },
   },
 };
