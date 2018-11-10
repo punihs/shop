e673db96e60bf413a8d230aa40a9081910cd5502
Manjesh V <manjeshpv@gmail.com>
2018-11-11 00:39:15 +0530

    fixed: sidebar

--- member/client/components/navigation/navigation.directive.js
+++ member/client/components/navigation/navigation.directive.js
@@ -1,8 +1,31 @@
 
-angular.module('uiGenApp')
-  .directive('navigation', () => ({
-    templateUrl: 'components/navigation/navigation.html',
-    restrict: 'E',
-    controller: 'NavigationController',
-    controllerAs: 'Navigation',
-  }));
+(() => {
+  class NavigationController {
+    /*  @ngInject  */
+    constructor($http, $timeout, Session) {
+      this.$http = $http;
+      this.$timeout = $timeout;
+      this.Session = Session;
+      this.$onInit();
+    }
+
+    $onInit() {
+      this.user = this.Session.read('userinfo');
+      this.states = this.Session.read('states');
+      this.active = 'calendar';
+    }
+
+    toggle(tab) {
+      this.active = tab === this.active ? null : tab;
+    }
+  }
+
+  angular.module('uiGenApp')
+    .directive('navigation', () => ({
+      templateUrl: 'components/navigation/navigation.html',
+      restrict: 'E',
+      controller: NavigationController,
+      controllerAs: '$ctrl',
+      bindToController: true,
+    }));
+})();
