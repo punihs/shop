66e4056fdf0444eddd6f8eb821635804c97b8c0f
Manjesh V <manjesh@quetzal.in>
2018-11-15 14:10:23 +0530

    buildscript (#2)

---
+++ scripts/staging.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+cd /home/pulse/shoppre/parcel;
+git reset --hard;
+git remote update origin;
+git checkout staging;
+git pull origin staging;
+
+# Parcel API
+npm install --production;
+npm run migrate;
+npm run seed;
+kill -HUP $(/usr/sbin/lsof -i:5007 -t);
+
+
+cd lambda;
+npm i --production;
+npm run email-deploy;
+kill -HUP $(/usr/sbin/lsof -i:5004 -t);
+cd ..
+
+cd chicken;
+npm i --production;
+npm run migrate;
+npm run seed;
+kill -HUP $(/usr/sbin/lsof -i:6000 -t);
