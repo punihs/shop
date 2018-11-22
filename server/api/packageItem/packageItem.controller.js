0b366e520da5fcb548c80b59ffb0358db4c8a7a3
Vikas Kumar <vikasjson@gmail.com>
2018-11-22 20:23:04 +0530

    refactor(Package Items): promise changed to async/await changed and try catch block added (#32)
    * ref(Package Items): Code refector for package items
    
    * refactor(PackageItem): code structur modified
    
    1. lastInsertedId changed to item
    2. Blank Line before block of code like await etc...
    
    * refactor(PackagesItems): given padd line for code blocks and model name packageItem changed to PackageItem

--- server/api/packageItem/packageItem.controller.js
+++ server/api/packageItem/packageItem.controller.js
@@ -15,108 +15,140 @@ const {
 
 const log = debug('api-packageItem-controller');
 
-exports.index = (req, res, next) => {
-  const options = {
-    attributes: ['id', 'name'],
-    limit: Number(req.query.limit) || 20,
-  };
+exports.index = async (req, res, next) => {
+  try {
+    const options = {
+      attributes: ['id', 'name'],
+      limit: Number(req.query.limit) || 20,
+    };
 
-  return PackageItem
-    .findAll(options)
-    .then(packages => res.json(packages))
-    .catch(next);
+    const packageItems = await PackageItem
+      .findAll(options);
+
+    return res.json(packageItems);
+  } catch (err) {
+    return next(err);
+  }
 };
 
 
 exports.create = async (req, res, next) => {
-  const packageItem = req.body;
-  const { base64: base64String, filename } = req.body.photo_file;
-  const extension = filename.split('.').pop();
-  if (!['jpg', 'jpeg', 'png', 'bmp'].includes(extension.toLowerCase())) {
-    return res.status(400).end('Invalid File');
+  try {
+    const packageItem = req.body;
+    const { base64: base64String, filename } = req.body.photo_file;
+    const extension = filename.split('.').pop();
+
+    if (!['jpg', 'jpeg', 'png', 'bmp'].includes(extension.toLowerCase())) {
+      return res.status(400).end('Invalid File');
+    }
+
+    packageItem.created_by = req.user.id;
+    packageItem.total_amount = packageItem.price_amount * packageItem.quantity;
+
+    if (packageItem.price) {
+      packageItem.price_entered_by = SHOPPRE;
+    }
+
+    const item = await PackageItem
+      .create(packageItem);
+
+    const object = `package_items/${item.id}/${item.id}_${moment().format('YYYY_MM_DD_h_mm_ss')}.${extension}`;
+
+    await minio
+      .base64Upload({
+        object,
+        base64String,
+      });
+
+    await item.update({ object });
+
+    return res.status(201).json({ item });
+  } catch (err) {
+    return next(err);
   }
+};
 
-  packageItem.created_by = req.user.id;
-  packageItem.total_amount = packageItem.price_amount * packageItem.quantity;
+exports.show = async (req, res, next) => {
+  try {
+    const packageItem = await PackageItem
+      .find({
+        attributes: ['id', 'quantity', 'price_amount', 'total_amount', 'object', 'object_advanced', 'name', 'object_invoice'],
+        where: { id: req.params.id },
+        include: [{
+          model: Package,
+          attributes: ['id'],
+        }, {
+          model: PackageItemCategory,
+          attributes: ['id', 'name'],
+        }],
+      });
 
-  if (packageItem.price) {
-    packageItem.price_entered_by = SHOPPRE;
+    return res.json(packageItem);
+  } catch (err) {
+    return next(err);
   }
-
-  return PackageItem
-    .create(packageItem)
-    .then((saved) => {
-      const { id } = saved;
-      const object = `package_items/${id}/${id}_${moment().format('YYYY_MM_DD_h_mm_ss')}.${extension}`;
-      minio
-        .base64Upload({
-          object,
-          base64String,
-        })
-        .then(() => saved.update({ object }));
-      return res.status(201).json({ id });
-    })
-    .catch(next);
 };
 
-exports.show = (req, res, next) => {
-  PackageItem
-    .find({
-      attributes: ['id', 'quantity', 'price_amount', 'total_amount', 'object', 'object_advanced', 'name', 'object_invoice'],
-      where: { id: req.params.id },
-      include: [{
-        model: Package,
-        attributes: ['id'],
-      }, {
-        model: PackageItemCategory,
-        attributes: ['id', 'name'],
-      }],
-    })
-    .then(packageitem => res.json(packageitem))
-    .catch(next);
+exports.destroy = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const status = await PackageItem.destroy({ where: { id } });
+
+    return res.json(status);
+  } catch (err) {
+    return next(err);
+  }
 };
 
-exports.destroy = async (req, res) => {
-  const { id } = req.params;
-  const status = await PackageItem.destroy({ where: { id } });
-  return res.json(status);
-};
+exports.update = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const packageItem = req.body;
+    packageItem.total = packageItem.price_amount * packageItem.quantity;
 
-exports.update = async (req, res) => {
-  const { id } = req.params;
-  const packageItem = req.body;
-  packageItem.total = packageItem.price_amount * packageItem.quantity;
-  const status = await PackageItem.update(packageItem, { where: { package_id: id } });
-  return res.json(status);
+    const status = await PackageItem.update(packageItem, { where: { package_id: id } });
+
+    return res.json(status);
+  } catch (err) {
+    return next(err);
+  }
 };
 
 
-exports.values = async (req, res) => {
-  const { id } = req.params;
-  let totalAmount = null;
-  req.body.forEach((x) => {
-    totalAmount += x.price_amount * x.quantity;
-    PackageItem
-      .update(
-        {
-          price_amount: x.price_amount,
-          quantity: x.quantity,
-          total_amount: x.price_amount * x.quantity,
-        },
-        { where: { id: x.id } },
-      );
-  });
-
-  Package.update({ price_amount: totalAmount }, { where: { id } });
-
-  packageService
-    .updateState({
-      pkg: { id },
-      actingUser: req.user,
-      nextStateId: IN_REVIEW,
-      comments: 'Cusotmer Updated/reset the value',
+exports.values = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    let totalAmount = null;
+    await req.body.forEach((x) => {
+      totalAmount += x.price_amount * x.quantity;
+      PackageItem
+        .update(
+          {
+            price_amount: x.price_amount,
+            quantity: x.quantity,
+            total_amount: x.price_amount * x.quantity,
+          },
+          { where: { id: x.id } },
+        );
     });
-  log('body', req.body);
-  return res.json({ message: 'Values updated succesfully' });
+
+    await Promise.all([
+      Package.update({ price_amount: totalAmount }, { where: { id } }),
+      packageService
+        .updateState({
+          pkg: { id },
+          actingUser: req.user,
+          nextStateId: IN_REVIEW,
+          comments: 'Cusotmer Updated/reset the value',
+        }),
+    ]);
+
+    log('body', req.body);
+
+    return res.json({ message: 'Values updated succesfully' });
+  } catch (err) {
+    return next(err);
+  }
 };
 
