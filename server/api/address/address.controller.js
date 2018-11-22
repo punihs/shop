b2f507af6c933ad241dcb9c946bddff218546d63
Meena <30926214+Meena111@users.noreply.github.com>
2018-11-22 14:12:35 +0530

    refactor(Address):  changed promise to async/await  (#27)
    * refactor: address
    
    * refactor(Address): space before return response
    
    * refactor(address): review changes
    
    * refactor(Address): space given,await added
    
    * fix(Address): changed Addresses to address

--- server/api/address/address.controller.js
+++ server/api/address/address.controller.js
@@ -1,102 +1,109 @@
 const _ = require('lodash');
-const debug = require('debug');
 
 const { GROUPS: { CUSTOMER } } = require('./../../config/constants');
 const { ajv, transform } = require('./../../components/ajv');
 const { Address, Country } = require('./../../conn/sqldb');
 
-const log = debug('s.api.address.controller');
-
-exports.index = (req, res, next) => {
-  const options = {
-    attributes: req.query.fl
-      ? req.query.fl.split(',')
-      : [
-        'id', 'customer_id', 'country_id', 'first_name', 'last_name', 'salutation',
-        'line1', 'line2', 'state', 'city', 'pincode', 'phone', 'is_default',
-      ],
-    where: {},
-    include: [{
-      model: Country,
-      attributes: ['iso2', 'name'],
-    }],
-    order: [['is_default', 'DESC'], ['created_at', 'DESC']],
-    limit: Number(req.query.limit) || 20,
-  };
-
-  if (req.user.group_id === CUSTOMER) {
-    options.where.customer_id = req.user.id;
-  } else if (req.query.id) {
-    options.where.customer_id = req.query.id;
-  }
-
-  return Address
-    .findAll(options)
-    .then(address => res.json(address))
-    .catch(next);
-};
-
-exports.show = (req, res, next) => {
-  const options = {
-    attributes: req.query.fl
-      ? req.query.fl.split(',')
-      : [
-        'id', 'customer_id', 'country_id', 'first_name', 'last_name', 'salutation',
-        'line1', 'line2', 'state', 'city', 'pincode', 'phone', 'is_default',
-      ],
-    where: { },
-    include: [{
-      model: Country,
-      attributes: ['name'],
-    }],
-  };
-
-  if (req.user.group_id === CUSTOMER) {
-    options.where.customer_id = req.user.id;
-    options.where.id = req.params.id;
-  }
-
-  return Address
-    .find(options)
-    .then(address => res.json(address))
-    .catch(next);
-};
-
-exports.create = (req, res, next) => {
-  log('create', req.body);
-  const valid = ajv.validate('createAddress', req.body);
-
-  if (!valid) {
-    return res
-      .status(400)
-      .json(transform(ajv));
-  }
-
-  const { is_default: isDefault } = req.body;
-
-  let promise = Promise.resolve();
-
-  if (isDefault) {
-    const where = {
-      is_default: true,
-      customer_id: req.user.id,
+exports.index = async (req, res, next) => {
+  try {
+    const options = {
+      attributes: req.query.fl
+        ? req.query.fl.split(',')
+        : [
+          'id', 'customer_id', 'country_id', 'first_name', 'last_name', 'salutation',
+          'line1', 'line2', 'state', 'city', 'pincode', 'phone', 'is_default',
+        ],
+      where: {},
+      include: [{
+        model: Country,
+        attributes: ['iso2', 'name'],
+      }],
+      order: [['is_default', 'DESC'], ['created_at', 'DESC']],
+      limit: Number(req.query.limit) || 20,
     };
 
-    promise = Address
-      .count({ where })
-      .then((found) => {
-        if (!found) return Promise.resolve();
-        return Address.update({ is_default: false }, { where });
-      });
-  }
+    if (req.user.group_id === CUSTOMER) {
+      options.where.customer_id = req.user.id;
+    } else if (req.query.id) {
+      options.where.customer_id = req.query.id;
+    }
 
-  return promise
-    .then(() => Address
+    const addresses = await Address
+      .findAll(options);
+
+    return res.json(addresses);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.show = async (req, res, next) => {
+  try {
+    const options = {
+      attributes: req.query.fl
+        ? req.query.fl.split(',')
+        : [
+          'id', 'customer_id', 'country_id', 'first_name', 'last_name', 'salutation',
+          'line1', 'line2', 'state', 'city', 'pincode', 'phone', 'is_default',
+        ],
+      where: {},
+      include: [{
+        model: Country,
+        attributes: ['name'],
+      }],
+    };
+
+    if (req.user.group_id === CUSTOMER) {
+      options.where.customer_id = req.user.id;
+      options.where.id = req.params.id;
+    }
+
+    const address = await Address
+      .find(options);
+
+    return res.json(address);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.create = async (req, res, next) => {
+  try {
+    const valid = ajv.validate('createAddress', req.body);
+
+    if (!valid) {
+      return res
+        .status(400)
+        .json(transform(ajv));
+    }
+
+    const { is_default: isDefault } = req.body;
+
+    let address = Promise.resolve({});
+
+    if (isDefault) {
+      const where = {
+        is_default: true,
+        customer_id: req.user.id,
+      };
+
+      address = await Address
+        .count({ where })
+        .then((found) => {
+          if (!found) return Promise.resolve();
+          return Address.update({ is_default: false }, { where });
+        });
+    }
+
+    const { id } = Address
       .create(Object.assign({}, req.body, {
         customer_id: req.user.id,
-      }))
-      .then(({ id }) => res.status(201).json({ id })))
-    .catch(next);
+      }));
+
+    return res.status(201).json({ id });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 exports.update = async (req, res, next) => {
@@ -116,10 +123,11 @@ exports.update = async (req, res, next) => {
 
     const status = await Address
       .update(_.omit(req.body, ['customer_id']), { where: { id } });
-    res
+
+    return res
       .json(status);
-  } catch (e) {
-    next(e);
+  } catch (err) {
+    return next(err);
   }
 };
 
@@ -135,9 +143,9 @@ exports.destroy = async (req, res, next) => {
         },
       });
 
-    res.json(address);
-  } catch (e) {
-    next(e);
+    return res.json(address);
+  } catch (err) {
+    return next(err);
   }
 };
 
