f2847674badc4eb0a18a1447b00dea20f456aea4
punihs <36591236+punihs@users.noreply.github.com>
2018-12-18 11:09:55 +0530

    fix(Try New): Onlick of try new button it will Redirected to New dashboard. (#114)

--- member/client/app/routes/access/oauth/oauth.controller.js
+++ member/client/app/routes/access/oauth/oauth.controller.js
@@ -27,7 +27,7 @@ class OAuthCtrl {
           // Used for updating session
           location.href = query.state
             ? `${location.origin}${query.state}`
-            : $state.href('packages.index', { absolute: true });
+            : $state.href('dashboard.index', { absolute: true });
         }))
       .catch(() => {
         location.href = URLS.OAUTH;
