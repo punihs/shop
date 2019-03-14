9424ec7bd267b595ecaee0c57e8fe7ac75963efa
pulse dev.shoppre.test <pulse@dev.shoppre.test>
2019-03-14 15:07:15 +0530

    fix(Shipment): Frst50 issue fixed

--- server/config/constants/index.js
+++ server/config/constants/index.js
@@ -101,7 +101,7 @@ const constants = {
     SHIPMENT_IN_ACTIVE: 72,
   },
   SHIPMENT_HISTORY: [24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 61],
-  SHIPMENT_COUNT: [23, 24, 25, 26, 27, 28, 29, 30, 31,
+  SHIPMENT_COUNT: [20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
     32, 33, 34, 35, 36, 37, 38, 39, 40],
 
   PRICE_ENTERER: {
