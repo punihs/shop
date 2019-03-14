dd0d8496067990a8200caa5f79a1167279da3866
pulse dev.shoppre.test <pulse@dev.shoppre.test>
2019-03-14 17:28:08 +0530

    feat(Dashboard): Popup given for update phone number

--- member/client/app/routes/dashboard/index/index.controller.js
+++ member/client/app/routes/dashboard/index/index.controller.js
@@ -1,9 +1,11 @@
 class DashboardIndexController {
-  constructor(Page, Session, URLS, $http) {
+  constructor(Page, Session, URLS, $http, $stateParams, toaster) {
     this.Page = Page;
     this.Session = Session;
     this.URLS = URLS;
     this.$http = $http;
+    this.$stateParams = $stateParams;
+    this.toaster = toaster;
 
     return this.$onInit();
   }
@@ -11,7 +13,15 @@ class DashboardIndexController {
   $onInit() {
     this.store = 'Amazon';
     this.user = this.Session.read('userinfo');
+
+    if (!this.user.phone) {
+      this.verifyPhone = true;
+    } else {
+      this.verifyPhone = false;
+    }
     this.currentOffer = {};
+    this.submitting = false;
+    this.data = {};
 
     this.Page.setTitle('Dashboard');
 
@@ -31,6 +41,62 @@ class DashboardIndexController {
       .get('https://cp.shoppre.com/offers/current.json/')
       .then((offer) => {
         this.currentOffer = offer.data;
+      });
+  }
+
+  updatePhoneNumber(newPhoneForm) {
+    const user = this.Session.read('userinfo');
+    const flag = this.update(newPhoneForm);
+    if (flag) {
+      user.phone = this.data.phone;
+      this.Session.create('userinfo', user);
+      this.verifyPhone = false;
+    }
+  }
+
+  validateForm(form) {
+    this.$stateParams.autofocus = '';
+    Object.keys(form).filter(x => !x.startsWith('$')).forEach((f) => {
+      if (form[f] && form[f].$invalid) {
+        if (!this.$stateParams.autofocus) this.$stateParams.autofocus = f;
+        form[f].$setDirty();
+      }
+    });
+    return form.$valid;
+  }
+
+  update(newPhoneForm) {
+    if (this.submitting) return null;
+    this.submitting = true;
+    this.clickUpload = true;
+
+    const form = this.validateForm(newPhoneForm);
+
+    const data = Object.assign({ }, this.data);
+    if (!form) return (this.submitting = false);
+
+    return this.$http
+      .put('/users/me', data)
+      .then(() => {
+        this.submitting = false;
+        this
+          .toaster
+          .pop('success', 'Phone Number Updated Successfully.', '');
+        return true;
+      })
+      .catch((err) => {
+        this.submitting = false;
+
+        const { field } = err.data;
+        newPhoneForm[err.data.field].$setValidity('required', false);
+        $(`input[name="${field}"]`)[0].focus();
+
+        this
+          .toaster
+          .pop('error', 'There was problem Updating Phone Number. Please contact Shoppre team.');
+
+        this.error = err.data;
+        return false;
       });
   }
 }
