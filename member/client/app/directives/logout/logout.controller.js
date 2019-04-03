a3f3439d7af43d095e7db61a5d8cd068156cca90
Punith H S <punith@shoppre.com>
2019-04-03 19:30:59 +0530

    fix(logout): logout issue with mobile is fixed

--- member/client/app/directives/logout/logout.controller.js
+++ member/client/app/directives/logout/logout.controller.js
@@ -7,24 +7,42 @@ class LogoutController {
     this.Auth = Auth;
   }
 
+  // $onInit() {
+  //   debugger;
+  //   this.notify = this.Session.read('notify');
+  //   this.user = this.Session.read('userinfo');
+  //
+  //   let onsignal = Promise.resolve();
+  //   if (this.Session.read('oneSignalPlayerId')) {
+  //     onsignal = this.$http
+  //       .delete(`#/notificationSubscriptions/${this.Session.read('oneSignalPlayerId')}?user_id=${this.user.id}`);
+  //   }
+  //
+  //   return onsignal
+  //     .then(() => {
+  //       return Promise.resolve();
+  //       return this.Auth
+  //         .logout()
+  //         .then(() => this.cleanup(), () => this.cleanup())
+  //     })
+  //     .catch(() => this.cleanup());
+  // }
+
   $onInit() {
     this.notify = this.Session.read('notify');
-    this.user = this.Session.read('userinfo');
-
-    let onsignal = Promise.resolve();
-    if (this.Session.read('oneSignalPlayerId')) {
-      onsignal = this.$http
-        .delete(`#/notificationSubscriptions/${this.Session.read('oneSignalPlayerId')}?user_id=${this.user.id}`);
+    if (!this.Session.read('oneSignalPlayerId')) {
+      return this.$http
+        .delete(`/notificationSubscriptions/${this.Session.read('oneSignalPlayerId')}`)
+        .then(() => this.Auth
+          .logout())
+        .then(() => this.cleanup(), () => this.cleanup())
+        .catch(() => this.cleanup());
+    } else {
+      return this.Auth
+        .logout()
+        .then(() => this.cleanup(), () => this.cleanup())
+        .catch(() => this.cleanup());
     }
-
-    return onsignal
-      .then(() => {
-        return Promise.resolve();
-        return this.Auth
-          .logout()
-          .then(() => this.cleanup(), () => this.cleanup())
-      })
-      .catch(() => this.cleanup());
   }
 
   cleanup() {
