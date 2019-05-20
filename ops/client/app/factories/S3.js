1a64dabdec5768fb3d062f51471e31e3f32e6b3b
Meena <30926214+Meena111@users.noreply.github.com>
2019-05-20 11:28:10 +0530

    fix(PS): file format given lowercase (#416)

--- ops/client/app/factories/S3.js
+++ ops/client/app/factories/S3.js
@@ -2,7 +2,7 @@ angular
   .module('uiGenApp')
   .factory('S3', ($http, toaster, URLS) => ({
     upload: (file, data, $ctrl) => $http
-      .get('/minio/presignedUrl', { params: { filename: file.name } })
+      .get('/minio/presignedUrl', { params: { filename: file.name.toLowerCase() } })
       .then(({ data: { object, url } }) => {
         const xhr = new XMLHttpRequest();
         xhr.open('PUT', url, true);
@@ -27,7 +27,7 @@ angular
       .catch(() => toaster.pop('error', 'Error while uploading file')),
 
     uploadAdvanced: (file, data, $ctrl) => $http
-      .get('/minio/presignedUrl', { params: { filename: file.name } })
+      .get('/minio/presignedUrl', { params: { filename: file.name.toLowerCase() } })
       .then(({ data: { object, url } }) => {
         const xhr = new XMLHttpRequest();
         xhr.open('PUT', url, true);
@@ -54,7 +54,7 @@ angular
       .catch(() => toaster.pop('error', 'Error while uploading file')),
 
     uploadInvoice: (file, data, $ctrl) => $http
-      .get('/minio/presignedUrl', { params: { filename: file.name } })
+      .get('/minio/presignedUrl', { params: { filename: file.name.toLowerCase() } })
       .then(({ data: { object, url } }) => {
         const xhr = new XMLHttpRequest();
         xhr.open('PUT', url, true);
@@ -81,7 +81,7 @@ angular
       .catch(() => toaster.pop('error', 'Error while uploading file')),
 
     uploadEcommerce: (file, data, $ctrl) => $http
-      .get('/minio/presignedUrl', { params: { filename: file.name } })
+      .get('/minio/presignedUrl', { params: { filename: file.name.toLowerCase() } })
       .then(({ data: { object, url } }) => {
         const xhr = new XMLHttpRequest();
         xhr.open('PUT', url, true);
