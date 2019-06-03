888c2786e1c45a7f15736ec0027c971d1aef3a5a
Meena <30926214+Meena111@users.noreply.github.com>
2019-06-03 15:11:37 +0530

    feat(Shipment Create): Shipping preference displaying (#436)

--- member/client/app/routes/shipRequests/create/create.controller.js
+++ member/client/app/routes/shipRequests/create/create.controller.js
@@ -14,6 +14,7 @@ class ShipRequestsCreateController {
 
   $onInit() {
     this.Page.setTitle('Shipment Request Create');
+    this.packageOptions = false;
 
     this.customer = [];
     this.charges = {};
